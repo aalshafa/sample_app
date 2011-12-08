ea69eddc7c5d1367551f035a31826d219d7f1a0e
Ayman Alshafai <ayman.alshafai@gmail.com>
2011-12-08 05:26:59 -0600

    Added user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,35 @@
 class UsersController < ApplicationController
-  before_filter :authenticate, :only => [:index, :edit, :update, :destroy]
+  before_filter :authenticate, :except => [:show, :new, :create]
   before_filter :correct_user, :only => [:edit, :update]
   before_filter :admin_user,   :only => :destroy
+  
+  def index
+    @users = User.paginate(:page => params[:page])
+    @title = "All users"
+  end
+  
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end
 
+  def following
+    @title = "Following"
+    @user = User.find(params[:id])
+    @users = @user.following.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+  
+  def followers
+    @title = "Followers"
+    @user = User.find(params[:id])
+    @users = @user.followers.paginate(:page => params[:page])
+    render 'show_follow'
+  end
+
   def new
-    @user = User.new
+    @user  = User.new
     @title = "Sign up"
   end
   
@@ -16,45 +37,32 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       sign_in @user
-      flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user
+      redirect_to @user, :flash => { :success => "Welcome to the Sample App!" }
     else
       @title = "Sign up"
       render 'new'
     end
   end
- 
+  
   def edit
     @title = "Edit user"
   end
-
+  
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(params[:user])
-      flash[:success] = "Profile updated."
-      redirect_to @user
+      redirect_to @user, :flash => { :success => "Profile updated." }
     else
       @title = "Edit user"
       render 'edit'
     end
   end
-  
-  def index
-    @title = "All users"
-    @users = User.paginate(:page => params[:page])
-  end
 
   def destroy
-    User.find(params[:id]).destroy
-    flash[:success] = "User destroyed."
-    redirect_to users_path
+    @user.destroy
+    redirect_to users_path, :flash => { :success => "User destroyed." }
   end
-  
+
   private
-    
-    def authenticate
-      deny_access unless signed_in?
-    end
 
     def correct_user
       @user = User.find(params[:id])
@@ -62,6 +70,7 @@ class UsersController < ApplicationController
     end
     
     def admin_user
-      redirect_to(root_path) unless current_user.admin?
+      @user = User.find(params[:id])
+      redirect_to(root_path) if !current_user.admin? || current_user?(@user)
     end
- end
+end
