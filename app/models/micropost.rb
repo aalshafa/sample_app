af96259e98d4728e97f3c41ec320c574a79d4f9f
Ayman Alshafai <ayman.alshafai@gmail.com>
2011-12-08 06:47:46 -0600

    Added user following

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,3 +1,15 @@
+# == Schema Information
+# Schema version: 20100829210229
+#
+# Table name: microposts
+#
+#  id         :integer         not null, primary key
+#  content    :string(255)
+#  user_id    :integer
+#  created_at :datetime
+#  updated_at :datetime
+#
+
 class Micropost < ActiveRecord::Base
   attr_accessible :content
   
@@ -7,4 +19,15 @@ class Micropost < ActiveRecord::Base
   validates :user_id, :presence => true
   
   default_scope :order => 'microposts.created_at DESC'
+  
+  scope :from_users_followed_by, lambda { |user| followed_by(user) }
+  
+  private
+  
+    def self.followed_by(user)
+      following_ids = %(SELECT followed_id FROM relationships
+                        WHERE follower_id = :user_id)
+      where("user_id IN (#{following_ids}) OR user_id = :user_id",
+            :user_id => user)
+    end
 end
